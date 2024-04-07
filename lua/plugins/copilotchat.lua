206906b4f8cd75339b679d8b24fdbc7809d87214
Steven Moon <smoon@presagegroup.com>
2024-04-06 20:06:33 -0400

    Copilot Chat

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true,
+      -- default window options
+      window = {
+        layout = 'float', -- 'vertical', 'horizontal', 'float'
+        -- Options below only apply to floating windows
+        relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
+        border = 'single', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        width = 0.8, -- fractional width of parent
+        height = 0.6, -- fractional height of parent
+        row = nil, -- row position of the window, default is centered
+        col = nil, -- column position of the window, default is centered
+        title = 'Copilot Chat', -- title of chat window
+        footer = nil, -- footer of chat window
+        zindex = 1, -- determines if window is on top or below other floating windows
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
